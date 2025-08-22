d4a387e27a665881516d6d784e5a853460f46401
Anastasiia Tabachnikova <nastiatab11@gmail.com>
2025-08-22 11:21:01 +0200

    Add files and completed task #1

---
+++ opp_1/opp1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    float grunnlinje = 4;
+    float høyde = 12;
+    float areal = (grunnlinje * høyde) / 2;
+
+    printf("%f", areal);
+    return 0;
+}
